c4b4faa98d278996a50ee595830e377e223e781f
Patricio Mardones <patriciomardones35@gmail.com>
2026-03-17 20:10:25 -0300

    mark 4

--- intro.py
+++ intro.py
@@ -1,2 +1,11 @@
 print("Hola inframundo___,,,__(o.o)__,,,___")
 #Esta instruccion despliega por la terminal
+#Declarar variables
+#Para crear una variable no puede ser en mayúsculas, no puede empezar con un número, no puede contener espacios, no puede contener caracteres especiales ni letras con tilde tampoco la ñ
+nom = ""
+
+#Entrada
+nom = input ("Ingrese nombre: ")
+
+#Salida
+print("Hola", nom, " bienvenido al inframundo__,,,___(0.0)__,,,___")
