e0aa08e7187f1dcd6ddee0c695051820de70b078
Patricio Mardones <patriciomardones35@gmail.com>
2026-03-17 23:23:42 -0300

    mark 7

--- intro.py
+++ intro.py
@@ -1,5 +1,3 @@
-print("Hola inframundo___,,,__(o.o)__,,,___")
-#Esta instruccion despliega por la terminal
 #Declarar variables
 #Para crear una variable no puede ser en mayúsculas, no puede empezar con un número, no puede contener espacios, no puede contener caracteres especiales ni letras con tilde tampoco la ñ
 nom = ""
@@ -15,9 +13,9 @@ print("Hola", nom, " bienvenido al inframundo__,,,___(0.0)__,,,___")
 #No pedir edad
 
 #Entrada
-nom: "Bienvenido al servicio de registro de protección del programa Secure-Tec. Soy HAX y te guiare en el proceso de tu solicitud. Por favor ingrese los siguientes datos para que su postulación quede registrada, a la brevedad uno de nuestros tecnicos evaluará su situación y las posibilidades de obtener un espacio en nuestros refugios nucleares"
-nom: input ("Bienvenido al servicio de registro de protección del programa Secure-Tec. Soy HAX y te guiare en el proceso de tu solicitud. Por favor ingrese los siguientes datos para que su postulación quede registrada, a la brevedad uno de nuestros tecnicos evaluará su situación y las posibilidades de obtener un espacio en nuestros refugios nucleares")
-nom completo: input ("Ingrese nombre completo")
+nom = "Bienvenido al servicio de registro de protección del programa Secure-Tec. Soy HAX y te guiare en el proceso de tu solicitud. Por favor ingrese los siguientes datos para que su postulación quede registrada, a la brevedad uno de nuestros tecnicos evaluará su situación y las posibilidades de obtener un espacio en nuestros refugios nucleares"
+nom = input ("Ingrese nacionalidad:") 
+
 
 #Salida
-print("nom")
+print("Bienvenido al servicio de registro de protección del programa Secure-Tec. Soy HAX y te guiare en el proceso de tu solicitud. Por favor ingrese los siguientes datos para que su postulación quede registrada, a la brevedad uno de nuestros tecnicos evaluará su situación y las posibilidades de obtener un espacio en nuestros refugios nucleares")
