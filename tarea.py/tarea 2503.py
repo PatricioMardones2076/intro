a5dcdec6d5d3c5281d514617f4af340da786ebc4
Patricio Mardones <patriciomardones35@gmail.com>
2026-03-25 22:21:02 -0300

    mark 26

--- tarea.py/tarea 2503.py
+++ tarea.py/tarea 2503.py
@@ -6,9 +6,7 @@ precio_individual_A = cerveza_A_valor / cerveza_A
 
 valor_total = int(input("Inserte precio total: "))
 precio_six_pack_cerveza_B = valor_total - cerveza_A_valor
-cerveza_B = int(input("Inserte cantidad de latas: "))
-cerveza_B_valor = int(input("Inserte valor del six pack: "))
-precio_individual_cerveza_B = cerveza_B_valor / cerveza_B
+precio_individual_cerveza_B = precio_six_pack_cerveza_B / 6
 
 print(f"{precio_individual_A} = {cerveza_A_valor} / {cerveza_A}")
-print(f"precio_individual_cerveza_B = {cerveza_B_valor} / {cerveza_B}")
+print(f"{precio_individual_cerveza_B} = {precio_six_pack_cerveza_B} / 6")
