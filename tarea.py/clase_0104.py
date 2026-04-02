5eeeca755dc9dc1d0f81e32fdd689bdbcbd14f39
Patricio Mardones <patriciomardones35@gmail.com>
2026-04-01 22:07:57 -0300

    mark_32

---
+++ tarea.py/clase_0104.py
@@ -0,0 +1,12 @@
+#ejercicios
+
+#buscar 20 sistemas de entradas y de salidas, y de cada uno de estos, debe tener subsistemas (2c/u)
+
+#condicion if
+#Sintaxis:
+#if condicion: pro >= 4.0
+print("Aprobado")
+
+#else:
+print("Reprobado")
+#Operadores racionales: > (mayor que), <(menor que), >=(mayor o igual), <=(menor o igual), == (igual), != (distinto)
