7cd54ecad58ac9a1f04f8636da156c21ac537cd5
Patricio Mardones <patriciomardones35@gmail.com>
2026-04-07 20:31:35 -0400

    mark_34

--- tarea.py/clase 1803/clase_0704.py
+++ tarea.py/clase 1803/clase_0704.py
@@ -3,9 +3,38 @@ edad = int (input("Ingrese su edad: "))
 if edad >= 18:
     print("Usted es mayor de edad")
 
-    if edad >= 130:
+if edad >= 130:
         print("Edad no valida, INGRESE SU EDAD REAL")
 
 else: print("Usted es menor de edad")
 
 #Leer cuento chino "Buena suerte Mala suerte"
+
+edad = 0
+edad = int (input("Ingrese su edad: "))
+if edad < 0:
+    print ("La edad no puede ser menor que cero")
+
+else:
+    if edad <18:
+        print("Usted es menor de edad")
+
+    if edad > 130:
+        print("Su edad supera el limite permitido")
+    else:
+        print("Usted es mayor de edad")    
+
+#Desarrolle un programa que permita ingresar 3 numeros y desplegar siempre el mayor: Haga simulaciones siempre con 3 numeros distintos
+
+primer_num = int(input("Ingrese el primer numero:"))
+segundo_num = int(input("Ingrese el segundo numero:"))
+tercer_num = int(input("Ingrese el tercer numero:"))
+if primer_num > segundo_num and primer_num > tercer_num:
+    print("El numero mayor es: ", primer_num)
+if segundo_num > tercer_num and segundo_num > primer_num:
+        print("El numero mayor es: ", segundo_num)
+if tercer_num > segundo_num and tercer_num > primer_num:
+        print("El numero mayor es: ", tercer_num)
+
+
+#Desarrolle un programa que permita ingresar 3 numeros y siempre los despliegue ascendentemente: Haga simulaciones siempre con 3 numeros distintos
---
+++ tarea.py/clase 1803/tester.py
@@ -0,0 +1,9 @@
+primer_num = int(input("Ingrese el primer numero:"))
+segundo_num = int(input("Ingrese el segundo numero:"))
+tercer_num = int(input("Ingrese el tercer numero:"))
+if primer_num > segundo_num and primer_num > tercer_num:
+    print("El numero mayor es: ", primer_num)
+if segundo_num > tercer_num and segundo_num > primer_num:
+        print("El numero mayor es: ", segundo_num)
+if tercer_num > segundo_num and tercer_num > primer_num:
+        print("El numero mayor es: ", tercer_num)
