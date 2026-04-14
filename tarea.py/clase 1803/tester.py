71539e201bdcfe53a56c363c22755d881faa6ee2
Patricio Mardones <patriciomardones35@gmail.com>
2026-04-14 19:04:26 -0400

    mark_38

--- tarea.py/clase 1803/tester.py
+++ tarea.py/clase 1803/tester.py
@@ -3,14 +3,26 @@ primer_num = int(input("Ingrese el primer numero:"))
 segundo_num = int(input("Ingrese el segundo numero:"))
 tercer_num = int(input("Ingrese el tercer numero:"))
 if primer_num > segundo_num:
- if primer_num > tercer_num:
-    if segundo_num > tercer_num:
-     print("Los numeros en orden descendente son:", primer_num, segundo_num, tercer_num) 
+  if primer_num > tercer_num:
+    if segundo_num < tercer_num:
+      print("Los numeros en orden descendente son:", primer_num, tercer_num, segundo_num)
+if primer_num > segundo_num:
+  if primer_num > tercer_num:
+    if segundo_num < tercer_num:
+      print("Los numeros en orden descendente son:", primer_num, tercer_num, segundo_num) 
 if segundo_num > primer_num:
  if segundo_num > tercer_num:
-    if primer_num > tercer_num:
+    if primer_num < tercer_num:
+      print("Los numeros en orden descendente son:", segundo_num, primer_num, tercer_num)
+if segundo_num > primer_num:
+ if segundo_num > tercer_num:
+    if primer_num < tercer_num:
       print("Los numeros en orden descendente son:", segundo_num, primer_num, tercer_num)
 if tercer_num > primer_num:
   if tercer_num > segundo_num:
-    if primer_num > segundo_num:
-     print("Los numeros en orden descendente son:", tercer_num, segundo_num, primer_num)
+    if primer_num < segundo_num:
+      print("Los numeros en orden descendente son:", tercer_num, primer_num, segundo_num)
+if tercer_num > primer_num:
+  if tercer_num < segundo_num:
+    if primer_num < segundo_num:
+      print("Los numeros en orden descendente son:", tercer_num, segundo_num, primer_num)
