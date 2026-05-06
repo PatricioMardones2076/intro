e334bcec282a7f52eebbf600dcf25db464a902e4
Patricio Mardones <patriciomardones35@gmail.com>
2026-05-05 20:08:43 -0400

    mark_49

---
+++ tarea.py/clase_0505.py
@@ -0,0 +1,15 @@
+#for x in range(3,14,2):
+#print(x) 
+
+import time
+
+
+while True:
+    h = 0
+    for h in range (0, 24, 1):
+        m = 0
+        for m in range (0, 60, 1):
+            s = 0
+            for s in range (0, 60, 1):
+             time.sleep(0.00002)
+    print("Fin")
