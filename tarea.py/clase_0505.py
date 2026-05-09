0bedbc6a0f019864c5ea642ef1d2c0fbccd4f9fb
Patricio Mardones <patriciomardones35@gmail.com>
2026-05-08 20:40:05 -0400

    mark_50

--- tarea.py/clase_0505.py
+++ tarea.py/clase_0505.py
@@ -4,12 +4,24 @@
 import time
 
 
-while True:
-    h = 0
-    for h in range (0, 24, 1):
-        m = 0
-        for m in range (0, 60, 1):
-            s = 0
-            for s in range (0, 60, 1):
-             time.sleep(0.00002)
-    print("Fin")
+#while True:
+#   for h in range(0, 24, 1):
+#      for m in range(0, 60, 1):
+#         for s in range(0, 60, 1):
+#            print(h, ":", m, ":", s)
+            #time.sleep(0.000000002)
+
+#Desarrolle un prgrama que permita ingresar 5 numeros y al finalizar desplegar cuantos numeros pares ingreso el usuario
+#Solo se deben considerar numeros positivos
+#usando ciclo for
+contador_pares = 0
+contador_numeros = 0
+for _ in range(5):
+    numero = int(input("Ingrese un número positivo: "))
+    if numero > 0:
+        if numero % 2 == 0:
+            contador_pares += 1
+        contador_numeros += 1
+    else:  
+        print("Por favor, ingrese un número positivo.")    
+print("Cantidad de números pares ingresados:", contador_pares)  
