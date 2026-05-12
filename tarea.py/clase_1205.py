941843d682db0b7ad335785f03f147fe83c268f3
Patricio Mardones <patriciomardones35@gmail.com>
2026-05-12 19:49:45 -0400

    mark_56

--- tarea.py/clase_1205.py
+++ tarea.py/clase_1205.py
@@ -1,15 +1,15 @@
-#Desarrolle una funcion, y la funcion recibe un argumento. La funcion retornara un verdadero si el numero que recibio es un numero perfecto
+#Desarrolle una funcion, y la funcion recibe un argumento. La funcion retornara un True si el numero que recibio es un numero perfecto
 def es_numero_perfecto(numero):
-    suma = 0
-    for x in range (1, numero):
-        if numero % x == 0:
-            suma = suma + x
-    return suma == numero
-# Solicitar al usuario que ingrese un número
-numero = int(input("Ingrese un número: "))
-# Verificar si el número es perfecto y mostrar el resultado
+    if numero < 1:
+        return False
+    
+    suma_divisores = sum(i for i in range(1, numero) if numero % i == 0)
+    
+    return suma_divisores == numero
+numero = int(input("Ingrese un número para verificar si es perfecto: "))
 if es_numero_perfecto(numero):
     print(f"{numero} es un número perfecto.")
-else:    print(f"{numero} no es un número perfecto.")
-# Ejemplo de uso
-print(es_numero_perfecto(6))  # Debería retornar True, ya que 6 es un número perfecto (1 + 2 + 3 = 6)
+    print(True)
+else:
+    print(f"{numero} no es un número perfecto.")
+    print(False)
