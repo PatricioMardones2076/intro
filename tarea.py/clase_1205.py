8756bb60bdadd6c674b7d581dcf010af2c2b44f0
Patricio Mardones <patriciomardones35@gmail.com>
2026-05-12 20:23:49 -0400

    mark_57

--- tarea.py/clase_1205.py
+++ tarea.py/clase_1205.py
@@ -13,3 +13,36 @@ if es_numero_perfecto(numero):
 else:
     print(f"{numero} no es un número perfecto.")
     print(False)
+
+#Que los primeros 5 numeros sean perfectos  
+
+#def es_perfecto(num):
+#    suma = 0
+
+#    for i in range(1, num , 1):
+#        if num % i == 0:
+#            suma = suma + i
+
+#    if suma == num:
+#        return True
+#    else:
+#        return False
+
+#def primeros_perfectos(n: int):
+#   mis_perfectos = []
+#   numero = 1
+#   cont = 1
+#   while cont <= 5:
+#      print(numero)
+#      if es_perfecto(numero):
+#         mis_perfectos.append(numero)
+#         cont = cont + 1
+
+#      numero = numero + 1
+
+#   return mis_perfectos
+
+#Las funciones nunca retorna print, nos sirve como traza
+# Siempre vamos a retornar una variable tipo str
+# Nunca una funcion debe incluir un input
+
--- tarea.py/tarea_profe.py
+++ tarea.py/tarea_profe.py
@@ -1,28 +1,27 @@
 #Desarrolle un programa que permita calcular la fecha de nacimiento a un solo digito la fecha de 2 personas y luego sume estas dos fechas para obtener su numero maestro
 def calcular_numero_maestro(dia, mes, anio):
-    # Sumar los dígitos del día, mes y año
     suma = sum(int(digito) for digito in str(dia) + str(mes) + str(anio))
     
-    # Reducir la suma a un solo dígito
+    
     while suma >= 10:
         suma = sum(int(digito) for digito in str(suma))
     
     return suma
-# Solicitar la fecha de nacimiento de la primera persona
+
 dia1 = int(input("Ingrese el día de nacimiento de la primera persona: "))
 mes1 = int(input("Ingrese el mes de nacimiento de la primera persona: "))
 anio1 = int(input("Ingrese el año de nacimiento de la primera persona: "))
-# Solicitar la fecha de nacimiento de la segunda persona
+
 dia2 = int(input("Ingrese el día de nacimiento de la segunda persona: "))
 mes2 = int(input("Ingrese el mes de nacimiento de la segunda persona: "))
 anio2 = int(input("Ingrese el año de nacimiento de la segunda persona: "))
-# Calcular el número maestro para cada persona
+
 numero_maestro1 = calcular_numero_maestro(dia1, mes1, anio1)
 numero_maestro2 = calcular_numero_maestro(dia2, mes2, anio2)
-# Sumar los números maestros de ambas personas
+
 numero_maestro_total = numero_maestro1 + numero_maestro2
-# Reducir el número maestro total a un solo dígito
+
 while numero_maestro_total >= 10:
     numero_maestro_total = sum(int(digito) for digito in str(numero_maestro_total))
-# Mostrar el número maestro total
+
 print("El número maestro total es:", numero_maestro_total)
